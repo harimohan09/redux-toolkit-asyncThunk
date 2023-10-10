078fad7984181773158d8555051d31c3fa1f00ca
harimohan <bishnoiharimohan@gmail.com>
2023-10-10 20:45:52 +0530

    provided store to app

--- redux-toolkit-asyncthunk/src/index.js
+++ redux-toolkit-asyncthunk/src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import { store } from "./redux/store";
+import { provider } from "react-redux";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <provider store={store}>
+            <App />
+        </provider>
     </React.StrictMode>
 );
