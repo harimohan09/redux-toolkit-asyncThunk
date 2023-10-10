44200a88931f5fc3eae34af819a9c663c0510070
harimohan <bishnoiharimohan@gmail.com>
2023-10-11 00:02:56 +0530

    used Components inside app.js

--- redux-toolkit-asyncthunk/src/App.js
+++ redux-toolkit-asyncthunk/src/App.js
@@ -1,8 +1,20 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import Account from "./components/Account";
+import Bonus from "./components/Bonus";
+import { useSelector } from "react-redux";
 
 function App() {
-    return <div className="App">hello</div>;
+    const points = useSelector((state) => state.bonus.points);
+    const amount = useSelector((state) => state.account.amount);
+    return (
+        <div>
+            <h1>Amount: {amount}</h1>
+            <h1>Points: {points}</h1>
+            <Account />
+            <Bonus />
+        </div>
+    );
 }
 
 export default App;
