8e3dd298574d6b326cb7acd51fde1ef409ec3128
harimohan <bishnoiharimohan@gmail.com>
2023-10-11 00:02:17 +0530

    Added code to dispatch action for comp.Amont&Bonus

--- redux-toolkit-asyncthunk/src/components/Account.js
+++ redux-toolkit-asyncthunk/src/components/Account.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { increment, incrementByAmount } from "../redux/slices/accountSlice";
+import { decrement } from "../redux/slices/accountSlice";
+// import { useSelector } from "react-redux";
 
 const Account = () => {
-    return <div>Account</div>;
+    const [value, setValue] = useState(0);
+    const dispatch = useDispatch();
+    console.log("value", value);
+    // const amount = useSelector((state) => state.account.amount);
+    return (
+        <div>
+            <h1>Account Component</h1>
+            {/* <h2>Total Amount : {amount}</h2> */}
+            <button onClick={() => dispatch(increment())}>
+                Increment in amount
+            </button>
+            <button onClick={() => dispatch(decrement())}>
+                Decrement in amount
+            </button>
+            <input
+                type="number"
+                placeholder="Only Number"
+                onChange={(e) => setValue(e.target.value)}
+            ></input>
+            <button onClick={() => dispatch(incrementByAmount(value))}>
+                Increment in amount
+            </button>
+        </div>
+    );
 };
 
 export default Account;
--- redux-toolkit-asyncthunk/src/components/Bonus.js
+++ redux-toolkit-asyncthunk/src/components/Bonus.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { increment } from "../redux/slices/bonusSlice";
+import { decrement } from "../redux/slices/bonusSlice";
+// import { useSelector } from "react-redux";
 
 const Bonus = () => {
-    return <div>Bonus</div>;
+    const dispatch = useDispatch();
+    // const points = useSelector((state) => state.bonus.points);
+    return (
+        <div>
+            <h1>Bonus Component</h1>
+            {/* <h2>totalPoints: {points}</h2> */}
+            <button onClick={() => dispatch(increment())}>
+                Increment in points
+            </button>
+            <button onClick={() => dispatch(decrement())}>
+                Decrement in points
+            </button>
+        </div>
+    );
 };
 
 export default Bonus;
