c6b682aa4dcb0f38499d38b35ccbb749ee9a1029
harimohan <bishnoiharimohan@gmail.com>
2023-10-11 00:31:03 +0530

    used thunk middleware for async operation

--- redux-toolkit-asyncthunk/src/redux/slices/accountSlice.js
+++ redux-toolkit-asyncthunk/src/redux/slices/accountSlice.js
@@ -1,4 +1,16 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const getUserAccount = createAsyncThunk(
+    "account/getUser",
+    async (userId, thunkAPI) => {
+        const response = await fetch(
+            ` http://localhost:8080/accounts/${userId}`
+        );
+        const data = await response.json();
+        console.log("dta", data);
+        return data.amount;
+    }
+);
 
 export const accountSlice = createSlice({
     name: "account",
@@ -16,6 +28,11 @@ export const accountSlice = createSlice({
             state.amount += Number(action.payload);
             // console.log("action", action.payload);
         },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(getUserAccount.fulfilled, (state, action) => {
+            state.amount = action.payload;
+        });
     },
 });
 
